0f4b77719bebebeb7dce47004397f0b46a6ceb14
wilfredlopez <cubamc@gmail.com>
2020-11-01 14:03:48 -0500

    added parse-to-regex

--- src/util/generatePath.ts
+++ src/util/generatePath.ts
@@ -1,23 +1,4 @@
-import pathToRegexp from 'path-to-regexp';
-
-export interface PathFunctionOptions {
-  pretty?: boolean;
-  encode?: (value: string, token: Key) => string;
-}
-
-export type Token = string | Key;
-export type Path = string | RegExp | Array<string | RegExp>;
-export type PathFunction = (data?: Object, options?: PathFunctionOptions) => string;
-
-export interface Key {
-  name: string | number;
-  prefix: string;
-  delimiter: string;
-  optional: boolean;
-  repeat: boolean;
-  pattern: string;
-  partial: boolean;
-}
+import pathToRegexp, { PathFunction } from './path-to-regexp';
 
 const cache: { [key: string]: PathFunction } = {};
 const cacheLimit = 10000;
