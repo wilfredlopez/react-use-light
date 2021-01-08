7abf84a8dff0bf4c9621a6bc77c74d47ad1f4b5e
wilfredlopez <cubamc@gmail.com>
2021-01-07 21:19:52 -0500

    exporting debounceFunction and throttle function

--- src/util/throttle.ts
+++ src/util/throttle.ts
@@ -14,110 +14,112 @@
  *
  * @returns {Function}  A new, throttled, function.
  */
-export default function  throttle(
+export default function throttle(
   delay: number,
-  noTrailing: boolean | Function | undefined = false,
+  noTrailing: Function | boolean | undefined = false,
   callback?: Function | boolean,
-  debounceMode?: any,
+  debounceMode?: boolean | Function,
 ) {
   /*
-	 * After wrapper has stopped being called, this timeout ensures that
-	 * `callback` is executed at the proper times in `throttle` and `end`
-	 * debounce modes.
-	 */
-  let timeoutID: NodeJS.Timeout | undefined | number;
-  let cancelled = false;
+   * After wrapper has stopped being called, this timeout ensures that
+   * `callback` is executed at the proper times in `throttle` and `end`
+   * debounce modes.
+   */
+  let timeoutID: NodeJS.Timeout | undefined | number
+  let cancelled = false
 
   // Keep track of the last time `callback` was executed.
-  let lastExec = 0;
+  let lastExec = 0
 
   // Function to clear existing timeout
   function clearExistingTimeout() {
     if (timeoutID) {
-      clearTimeout(timeoutID as NodeJS.Timeout);
+      clearTimeout(timeoutID as NodeJS.Timeout)
     }
   }
 
   // Function to cancel next exec
   function cancel() {
-    clearExistingTimeout();
-    cancelled = true;
+    clearExistingTimeout()
+    cancelled = true
   }
 
   // `noTrailing` defaults to falsy.
   if (typeof noTrailing !== "boolean") {
-    debounceMode = callback;
-    callback = noTrailing;
-    noTrailing = undefined;
+    debounceMode = callback
+    callback = noTrailing
+    noTrailing = undefined
   }
 
   /*
-	 * The `wrapper` function encapsulates all of the throttling / debouncing
-	 * functionality and when executed will limit the rate at which `callback`
-	 * is executed.
-	 */
-  function wrapper(...arguments_): void {
-    //@ts-ignore
-    let self = this;
-    let elapsed = Date.now() - lastExec;
+   * The `wrapper` function encapsulates all of the throttling / debouncing
+   * functionality and when executed will limit the rate at which `callback`
+   * is executed.
+   */
+  function wrapper(this: any, ...args: any[]): void {
+    let self: any = this as any
+    let elapsed = Date.now() - lastExec
 
     if (cancelled) {
-      return;
+      return
     }
 
     // Execute `callback` and update the `lastExec` timestamp.
     function exec() {
-      lastExec = Date.now();
+      lastExec = Date.now()
       if (typeof callback === "function") {
-        callback.apply(self, arguments_);
+        callback.apply(self, args)
       }
     }
 
     /*
-		 * If `debounceMode` is true (at begin) this is used to clear the flag
-		 * to allow future `callback` executions.
-		 */
+     * If `debounceMode` is true (at begin) this is used to clear the flag
+     * to allow future `callback` executions.
+     */
     function clear() {
-      timeoutID = undefined;
+      timeoutID = undefined
     }
 
     if (debounceMode && !timeoutID) {
       /*
-			 * Since `wrapper` is being called for the first time and
-			 * `debounceMode` is true (at begin), execute `callback`.
-			 */
-      exec();
+       * Since `wrapper` is being called for the first time and
+       * `debounceMode` is true (at begin), execute `callback`.
+       */
+      exec()
     }
 
-    clearExistingTimeout();
+    clearExistingTimeout()
 
     if (debounceMode === undefined && elapsed > delay) {
       /*
-			 * In throttle mode, if `delay` time has been exceeded, execute
-			 * `callback`.
-			 */
-      exec();
+       * In throttle mode, if `delay` time has been exceeded, execute
+       * `callback`.
+       */
+      exec()
     } else if (noTrailing !== true) {
       /*
-			 * In trailing throttle mode, since `delay` time has not been
-			 * exceeded, schedule `callback` to execute `delay` ms after most
-			 * recent execution.
-			 *
-			 * If `debounceMode` is true (at begin), schedule `clear` to execute
-			 * after `delay` ms.
-			 *
-			 * If `debounceMode` is false (at end), schedule `callback` to
-			 * execute after `delay` ms.
-			 */
+       * In trailing throttle mode, since `delay` time has not been
+       * exceeded, schedule `callback` to execute `delay` ms after most
+       * recent execution.
+       *
+       * If `debounceMode` is true (at begin), schedule `clear` to execute
+       * after `delay` ms.
+       *
+       * If `debounceMode` is false (at end), schedule `callback` to
+       * execute after `delay` ms.
+       */
       timeoutID = setTimeout(
         debounceMode ? clear : exec,
         debounceMode === undefined ? delay - elapsed : delay,
-      );
+      )
     }
   }
 
-  wrapper.cancel = cancel;
+  wrapper.cancel = cancel
 
   // Return the wrapper function.
-  return wrapper;
+  return wrapper
 }
+
+
+
---
+++ src/util/throttleFunction.ts
@@ -0,0 +1,26 @@
+export function throttleFunction<T extends (...args: any[]) => any>(
+    fn: T,
+    threshhold: number = 250,
+    scope?: any,
+) {
+    threshhold || (threshhold = 250)
+    let last: number, deferTimer: NodeJS.Timeout
+    return function () {
+        var context: Window | any = scope || window // this was actually "this"
+
+        var now = +new Date(),
+            args: any[] = arguments as any
+        if (last && now < last + threshhold) {
+            // hold on to it
+            clearTimeout(deferTimer)
+            deferTimer = setTimeout(function () {
+                last = now
+                fn.apply(context, args)
+            }, threshhold)
+        } else {
+            last = now
+            fn.apply(context, args)
+        }
+    } as T
+}
+
